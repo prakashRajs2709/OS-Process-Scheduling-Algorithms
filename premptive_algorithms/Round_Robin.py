33bb69746282b1e1ed34a4e5df9b38916ac80ccf
prakashRajs2709 <rajsp6037@gmail.com>
2025-10-23 11:22:23 +0530

    feat: changed the name of the folder and few changes in the code of round robin

--- premptive_algorithms/Round_Robin.py
+++ premptive_algorithms/Round_Robin.py
@@ -13,6 +13,7 @@ class Round_Robin:
         TQ = self.TQ
         n = len(AT)
         mylst = []
+        total_tat = total_wt = total_ret = 0
         for i in range(len(AT)):
             mylst.append(
                 {
@@ -20,9 +21,11 @@ class Round_Robin:
                     "AT": AT[i],
                     "BT": BT[i],
                     "RT": BT[i],
+                    "first_start_time": -1,
                     "CT":0,
                     "TAT":0,
-                    "WT":0
+                    "WT":0,
+                    "ReT":0,
                 }
             )
         mylst.sort(key=lambda x:x['AT'])
@@ -37,6 +40,8 @@ class Round_Robin:
             
             if ready_queue:
                 cur_process = ready_queue.popleft()
+                if cur_process['first_start_time']==-1:
+                    cur_process['first_start_time']=ct
                 time_slice = min(TQ,cur_process['RT'])
                 ct+=time_slice
                 cur_process['RT']-=time_slice
@@ -57,13 +62,27 @@ class Round_Robin:
         for process in completed_processes:
             process['TAT'] = process['CT'] - process['AT']
             process['WT'] = process['TAT'] - process['BT']
-
-        print(f"PID|||AT|||BT|||CT|||TAT|||WT")
+            process['ReT'] = process['first_start_time'] - process['AT']
+            total_tat+=process['TAT']
+            total_wt+=process['WT']
+            total_ret+=process['ReT']
+        
+        print(f"PID|||AT|||BT|||CT|||TAT|||WT|||ReT")
         for process in mylst:
-            print(f"{process['pid']}|||||{process['AT']}||||{process['BT']}||||{process['CT']}|||||{process['TAT']}||||{process['WT']}")
+            print(f"{process['pid']}|||||{process['AT']}||||{process['BT']}||||{process['CT']}|||||{process['TAT']}||||{process['WT']}|||||{process['ReT']}")
+        return f"Average Turn Around Time: {str(round(total_tat/len(self.AT),2))}, Average Waiting Time: {round(total_wt/len(self.BT),2)}, Average Response Time: {round(total_ret/len(self.BT),2)}"
 
 
 
 
-obj = Round_Robin(pid = [1,2,3],AT = [0,4,5], BT = [5,2,4], TQ = 2)
-obj.solve()
+# obj = Round_Robin(pid = [1,2,3],AT = [0,4,5], BT = [5,2,4], TQ = 2)
+# print(obj.solve())
+
+# obj = Round_Robin(pid = [1,2,3,4,5],AT = [0,1,2,3,4], BT = [5,3,1,2,3], TQ = 2)
+# print(obj.solve())
+
+# obj = Round_Robin(pid = [1,2,3,4],AT = [0,1,2,4], BT = [5,4,2,1], TQ = 2)
+# print(obj.solve())
+
+obj = Round_Robin(pid = [1,2,3,4,5,6],AT = [0,1,2,3,4,6], BT = [4,5,2,1,6,3], TQ = 2)
+print(obj.solve())
